56bf9b4a7ad4948c5cd2c508faed00b484e7aa32
Karamel <120777071+Shimpukalikeee@users.noreply.github.com>
2025-03-07 04:09:30 +0800

    Update login.php

--- login.php
+++ login.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Login - AU Technical Support</title>
-    <link rel="icon" type="image/png" href="img/logo.png">
+    <link rel="icon" type="image/png" href="logo.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="styles.css">
 </head>
