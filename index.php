eb8c911f9d8e7cb3cc962aab9f40b113a16d4258
Karamel <120777071+Shimpukalikeee@users.noreply.github.com>
2025-03-07 04:08:30 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>AUJRC</title>
-    <link rel="icon" type="image/png" href="img/logo.png">
-    <link rel="stylesheet" href="assets/style2.css">
+    <link rel="icon" type="image/png" href="logo.png">
+    <link rel="stylesheet" href="style2.css">
     <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 </head>
 <body>
